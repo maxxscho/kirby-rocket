6e1f92edcab1014daf64bf011e624cc807a4db65
Markus Schober <maxx.schober@gmail.com>
2024-09-28 18:28:40 +0200

    Install tailwindcss

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,16 @@
-<h1><?= $page->title() ?></h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+    <?= vite([
+      'src/css/app.css',
+      'src/js/app.js',
+    ]) ?>
+  </head>
+  <body>
+    <h1><?= $page->title() ?></h1>
+  </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './site/**/*.php',
+    './site/**/*.yml',
+    './content/**/*.txt'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
